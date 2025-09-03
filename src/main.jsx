bbda9e4ae92dfe2fd64e15b70d201fe11d0e77a4
Elmer <abongnui381@gmail.com.com>
2025-09-03 13:14:47 +0100

    messafe

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter, } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import "./i18n"; 
@@ -8,8 +8,8 @@ import "./i18n";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
